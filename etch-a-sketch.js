dad12a297665e89c1801c90be56a189fb69b19c6
Cole <coleb@outlook.com>
2020-07-30 13:44:01 -0600

    Finished the reset and the rgb buttons

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,12 @@
 const container = document.querySelector('#container');
+const btnReset = document.querySelector('#reset');
+const btnRgb = document.querySelector('#rgb');
+let rows = 16;
+let columns = 16;
+let rgbOn = false
+let root = document.documentElement;
 
 function createGrid(rows, columns) {
-    let root = document.documentElement;
     root.style.setProperty('--rows', rows);
     root.style.setProperty('--columns', columns);
     for (let i = 0; i < columns; i++) {
@@ -17,8 +22,27 @@ function createGrid(rows, columns) {
 }
 
 
-createGrid(16, 16);
+function reset() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    rows = prompt("How many rows would you like?");
+    columns = prompt("How many columns would you like?");
+    createGrid(rows, columns);
+}
 
+function rgb() {
+    if (rgbOn) {
+        rgbOn = false;
+        root.style.setProperty('--color', lightgray);
+    } else {
+        rbgOn = true;
+        root.style.setProperty('--color', '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6));
+    }
 
+}
 
+btnReset.addEventListener('click', reset);
+btnRgb.addEventListener('click', rgb);
 
+createGrid(rows, columns);
